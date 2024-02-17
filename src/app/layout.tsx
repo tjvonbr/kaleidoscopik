1280c7e3bad79438072db807ed81223cb4d5dd84
Trevor Von Bruenchenhein <tjvonbr@gmail.com>
2024-02-17 15:50:50 -0700

    clerk install and firebase & next-auth removal

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,10 @@
 import { Header } from "@/components/Header";
+import { ClerkProvider } from "@clerk/nextjs";
 import "./globals.css";
 import { Metadata } from "next";
 
 export const metadata: Metadata = {
-  title: "Libello",
+  title: "wetbrush",
   description: "Create your own AI-gnerated coloring books",
 };
 
@@ -13,11 +14,13 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
-      <body>
-        <Header />
-        {children}
-      </body>
-    </html>
+    <ClerkProvider>
+      <html lang="en">
+        <body>
+          <Header />
+          {children}
+        </body>
+      </html>
+    </ClerkProvider>
   );
 }
